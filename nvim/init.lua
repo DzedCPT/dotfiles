7b89384abbee45d4c0ab90f9668123e925522c1e
DzedCPT <jedda@plumerai.com>
2024-03-22 10:17:58 +0100

    nvim: add tabs

--- nvim/init.lua
+++ nvim/init.lua
@@ -108,6 +108,20 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	-- Basic util plugins
+	{'romgrk/barbar.nvim',
+    	-- dependencies = {
+     --  	  'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+     --  	  'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    	-- },
+    	init = function() vim.g.barbar_auto_setup = false end,
+    	opts = {
+      	  -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      	  -- animation = true,
+      	  -- insert_at_start = true,
+      	  -- …etc.
+    	},
+    	version = '^1.0.0', -- optional: only update when a new 1.x version is released
+	},
 	-- Add surround actions
 	"echasnovski/mini.surround",
 	-- Need this for copying text from remotes to local clipboard
