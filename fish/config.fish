278731ff5f2be40e274c0d5de5d5a5bfe67c97ba
DzedCPT <jedda@plumerai.com>
2024-03-29 13:44:43 +0000

    fish: use c+i to keep selection.

--- fish/config.fish
+++ fish/config.fish
@@ -58,6 +58,7 @@ alias dspace="sudo du -hsx /* | sort -rh | head -n 40"
 alias sz="du -sh */ | sort -h"
 alias node-pools="gcloud container node-pools list --cluster=ponos-cluster --location=europe-west4-a | cut -f1 -d' '"
 bind \cV 'edit_cmd'
+bind \ci accept-autosuggestion
 alias klog='tail -f /var/log/kern.log'
 
 function vm; 
