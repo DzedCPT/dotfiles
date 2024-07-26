100ad461bad5e754fd6fde5edb9b243da7c1d517
DzedCPT <accounts@jeddaboyle.com>
2024-07-26 23:08:15 +0200

    nvim: base plugins

---
+++ nvim/lua/plugins/utils.lua
@@ -0,0 +1,7 @@
+return {
+    {"DzedCPT/nvim-rooter", opts={}},
+    {"nvim-tree/nvim-web-devicons", opts={}},
+    {"ojroques/nvim-osc52", opts={}}, -- Need this for copying text from remotes to local clipboard
+    {"gbprod/substitute.nvim", opts={}},
+    "machakann/vim-highlightedyank",
+}
