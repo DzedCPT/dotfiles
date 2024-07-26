dc7f02e75aa8e7cc8aeef3fcdf58608e9b866a24
DzedCPT <accounts@jeddaboyle.com>
2024-07-27 00:28:38 +0200

    nvim: add util plugin configs

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -1,7 +1,39 @@
+require("helpers")
+
 return {
-    {"DzedCPT/nvim-rooter", opts={}},
-    {"nvim-tree/nvim-web-devicons", opts={}},
-    {"ojroques/nvim-osc52", opts={}}, -- Need this for copying text from remotes to local clipboard
-    {"gbprod/substitute.nvim", opts={}},
-    "machakann/vim-highlightedyank",
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 10000, -- Make sure this loads before anything else
+		opts = {
+			-- This remove's gutter's background color
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
+		},
+		init = function()
+			local color_scheme = getDarkMode() == "Dark" and "kanagawa-lotus" or "kanagawa-dragon"
+			vim.cmd("colorscheme " .. color_scheme)
+		end,
+	},
+	{ "DzedCPT/nvim-rooter", opts = {} },
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
+	{
+		"ojroques/nvim-osc52", -- Need this for copying text from remotes to local clipboard
+		lazy = true,
+		opts = {},
+		init = function()
+			bind("n", "<leader>y", require("osc52").copy_operator, { expr = true })
+			bind("v", "<leader>y", require("osc52").copy_visual)
+			bind("n", "<leader>yy", "<leader>y_", { remap = true })
+		end,
+	},
+	{ "gbprod/substitute.nvim", opts = {} },
+	"machakann/vim-highlightedyank",
 }
