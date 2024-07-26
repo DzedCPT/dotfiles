08e249f138d10131d354156c3f3bc5bdfb48d42f
DzedCPT <accounts@jeddaboyle.com>
2024-07-26 23:08:34 +0200

    nvim: telescope

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,28 @@
+local function builtin()
+	return require("telescope.builtin")
+end
+
+return {
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{ "<leader>o", builtin().git_files },
+		{
+			"<leader>b",
+			function()
+				builtin().buffers({ ignore_current_buffer = true, sort_lastused = true })
+			end,
+		},
+		-- ZZZ: Maybe this should be <c-o>
+		{ "<leader>o", builtin().git_files },
+		{ "<leader>fo", builtin().find_files },
+		{ "<leader>ff", builtin().live_grep },
+		{ "<leader>s", builtin().lsp_document_symbols },
+		{ "<leader>fj", builtin().jumplist },
+		{ "<leader>fs", builtin().grep_string },
+		{ "<leader>fp", builtin().resume },
+		{ "<leader>fh", builtin().help_tags },
+		{ "<leader>fa", builtin().commands },
+	},
+}
