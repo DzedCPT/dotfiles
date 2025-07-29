045ac7d569eaed1172ac8a5fedb6bc0ef7c9fda1
DzedCPT <accounts@jeddaboyle.com>
2025-07-29 21:14:45 +0200

    Format init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -20,28 +20,28 @@ require("opts")
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- ZZZ: What to do about this?
-  change_detection = {
-    -- automatically check for config file changes and reload the ui
-    enabled = false,
-    notify = false, -- get a notification when changes are found
-  },
-  rocks = {
-  -- Set to true if you want to use the lua package manager luarocks.
-  -- Currently I don't use any packages that require luarocks, so disable it to 
-  -- avoid needing it as a dependency.
-      enabled = false,
-      hererocks = false
-    },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { vim.g.colors_name } },
-  -- Don't automatically check for plugin updates
-  checker = { enabled = false },
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- ZZZ: What to do about this?
+	change_detection = {
+		-- automatically check for config file changes and reload the ui
+		enabled = false,
+		notify = false, -- get a notification when changes are found
+	},
+	rocks = {
+		-- Set to true if you want to use the lua package manager luarocks.
+		-- Currently I don't use any packages that require luarocks, so disable it to
+		-- avoid needing it as a dependency.
+		enabled = false,
+		hererocks = false,
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { vim.g.colors_name } },
+	-- Don't automatically check for plugin updates
+	checker = { enabled = false },
 })
 
 require("keybindings")
@@ -49,4 +49,3 @@ require("autocmds")
 require("cmds")
 require("statusline")
 require("diagnostic_display").setup()
-
